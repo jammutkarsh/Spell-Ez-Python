714901f3e4b672ed8f7c6d87ee1ea84629219860
JammUtkarsh <5.utkarshchourasia@gmail.com>
2021-09-17 11:14:05 +0530

    fix: test version available.

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import random as r
 import tts  # tts.py
 import urllib3
-from os import system
 
 # To disable the error below.
 #   InsecureRequestWarning: Unverified HTTPS request is being made to host 'translate.google.com'.
